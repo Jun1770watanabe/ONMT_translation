18c83b0689736ec4451add88fed6b38cc4cdac5e
Jun Watanabe <38196649+Jun1770watanabe@users.noreply.github.com>
2020-11-25 15:07:31 +0900

    201125
    command

--- command.sh
+++ command.sh
@@ -1,6 +1,4 @@
-python \
-    translate.py \
-    -model ./model/200326_wmt17_2000_step_500000.pt \
-    -src ./txt/MCS/test_p1be.nm \
-    -tgt ./txt/wmt17/test.en \
+python translate.py -model ./model/200326_wmt17_2000_step_500000.pt -src ./txt/wmt17/test.nm -tgt ./txt/wmt17/test.en > log/log_wmt17_2000.txt \
+python translate.py -model ./model/200326_wmt17_280_step_100000.pt -src ./txt/wmt17/test.nm -tgt ./txt/wmt17/test.en > log/log_wmt17_280.txt \
+python translate.py -model ./model/JESC_en_blstm_step_200000.pt -src ./txt/JESC/en_test.nm -tgt ./txt/JESC/test.en > log/log_JESC_en.txt /
 
