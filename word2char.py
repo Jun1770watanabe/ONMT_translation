0808cc8e446a9bdef9b966fd0144f5de787904ae
Jun Watanabe <38196649+Jun1770watanabe@users.noreply.github.com>
2021-01-22 00:55:04 +0900

    210121_update

--- word2char.py
+++ word2char.py
@@ -13,14 +13,11 @@ args = argparser()
 with open(args.input_path, encoding="utf-8") as f:
     sentences = f.readlines()
 
-sentences = [s.replace(" ", "") for s in sentences]  
-
-# sentences = [list(s.replace(" ", "")) for s in sentences]  
-# sentences = [" ".join(s) for s in sentences]
-
-output = os.path.splitext(os.path.basename(args.input_path))[0]
+sentences = [" ".join(list(s)) for s in sentences]  
+# output = os.path.splitext(os.path.basename(args.input_path))[0]
 # output = "result/" + output + "_char.jp"
-output = "result/for_bleu/" + output + ".jp"
+# output = "result/for_bleu/" + output + ".jp"
+output = args.input_path
 
 with open(output, 'w', encoding="utf-8") as f:
     f.write("".join(sentences))
